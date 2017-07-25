db7644fe967b56b3564e0957052bc574fc7fcbe5
ntgayathri <ntgayathri@gmail.com>
2017-07-25 16:03:43 +0530

    promise
    javascript - promise

---
+++ script.js
@@ -0,0 +1,35 @@
+if(window.Promise){
+      console.log('Promise found');
+      var promise=new Promise(function(resolve,reject){
+            var request = new XMLHttpRequest();
+            request.open('GET', 'http://api.icndb.com/jokes/random');
+            request.onload = function() {
+              if (request.status == 200) {
+                resolve(request.response); //we get the data here.So, resolve the Promise
+				console.log(request.response);
+              }
+              else {
+                reject(Error(request.statusText)); //if status is not 200 OK, reject.
+              }
+            };
+        
+            request.onerror = function() {
+              reject(Error("Error fetching data.")); //error occurred, reject the Promise
+            };
+        
+            request.send(); //send the request
+      });
+              
+      console.log('Asynchronous request made.');
+    
+      promise.then(function(data){
+            console.log('Got data! Promise fulfilled.');
+            document.getElementsByTagName('body')[0].textContent=JSON.parse(data).value.joke;
+          },function(error){
+            console.log('Promise rejected.');
+            console.log(error.message);
+      });
+
+    }
+    else
+        console.log('Promise not available');
